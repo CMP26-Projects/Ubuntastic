5aa42ebef6d44c41f2e23fbfe5b7f67b957e52a3
Mariam Amin <maraimaminramadan2003@gmail.com>
2024-04-08 12:29:41 +0200

    communication

--- PhaseOne/process_generator.c
+++ PhaseOne/process_generator.c
@@ -1,27 +1,128 @@
 #include "headers.h"
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+#include<sys/types.h>
+#include<stdlib.h>
+#include<string.h>
+#include<signal.h>
 
-void clearResources(int);
+
+struct schdularType
+{
+    int mtype;
+    char schedType[70];
+
+};
+struct processData
+{
+
+    int id, arrival, runtime ,priority;
+    
+};
+
+struct messageProcess
+{
+    int mtype;
+    struct processData Process;
+
+};
+
+
+int createMessageQueue1()
+{
+    key_t key_id;
+    int msgid,recval;
+
+    key_id=ftok("file.txt",65);
+    msgid=msgget(key_id,0666|IPC_CREAT);
+
+    if(msgid==-1)
+    {
+        perror("there error in creates a queue !");
+        exit(-1);
+    }
+    printf("Message queue ID is : %d \n",msgid);
+    return msgid;
+}
+
+int creatShMemory()
+{
+    int key=ftok("file.txt",67);
+      int shmid = shmget(key, 50, IPC_CREAT | 0666);
+    if ((long)shmid == -1)
+    {
+        perror("Error in creating shm!");
+        exit(-1);
+    }
+   
+    return shmid;
+}
+
+//void clearResources(int);
 
 int main(int argc, char * argv[])
 {
-    signal(SIGINT, clearResources);
+    //signal(SIGINT, clearResources);
     // TODO Initialization
     // 1. Read the input files.
     // 2. Ask the user for the chosen scheduling algorithm and its parameters, if there are any.
     // 3. Initiate and create the scheduler and clock processes.
     // 4. Use this function after creating the clock process to initialize clock
-    initClk();
+   // initClk();
     // To get time use this
-    int x = getClk();
-    printf("current time is %d\n", x);
+   // int x = getClk();
+   // printf("current time is %d\n", x);
+
+    struct schdularType schedularmessage;
+    int done;
+    strcpy(schedularmessage.schedType,"RR");
+    
+    int msgid1=createMessageQueue1();
+    done =msgsnd(msgid1,&schedularmessage,sizeof(schedularmessage.schedType),!IPC_NOWAIT);
+   
+    
+    
+    if(done==-1)
+    {
+        perror("error in reciving ?");
+
+    }
+  struct processData *P = (struct processData*)malloc(sizeof(struct processData));
+  P->arrival=1;
+  P->id=2;
+  P->priority=3;
+  P->runtime=4;
+
+int shmid=creatShMemory();
+
+void *shmaddr = shmat(shmid, NULL, 0);
+  if (shmaddr == (void *)-1)
+  {
+    perror("Error in attach in writer");
+    exit(-1);
+  }
+
+  printf("\nWriter: Shared memory attached at address %p\n", shmaddr);
+  memcpy(shmaddr, P, sizeof(struct processData));
+
+
+shmdt(shmaddr);
+
+
+
+
+
+return 0;
+
     // TODO Generation Main Loop
     // 5. Create a data structure for processes and provide it with its parameters.
     // 6. Send the information to the scheduler at the appropriate time.
     // 7. Clear clock resources
-    destroyClk(true);
+   // destroyClk(true);
 }
 
-void clearResources(int signum)
-{
+//void clearResources(int signum)
+//{
     //TODO Clears all resources in case of interruption
-}
+//}
--- PhaseOne/scheduler.c
+++ PhaseOne/scheduler.c
@@ -1,12 +1,124 @@
 #include "headers.h"
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
 
-
-int main(int argc, char * argv[])
+struct schdularType
 {
-    initClk();
-    
-    //TODO implement the scheduler :)
-    //upon termination release the clock resources.
-    
-    destroyClk(true);
+    int mtype;
+    char schedType[70];
+};
+struct processData
+{
+
+    int id, arrival, runtime, priority;
+};
+
+void HandlerINT(int signum)
+{
+}
+void HandlerCHILD(int signum)
+{
+}
+void generateLogFile()
+{
+}
+void generatePrefFile()
+{
+}
+void createProcess()
+{
+}
+void getState()
+{
+}
+void updateOutfile()
+{
+}
+
+int createMessageQueue()
+{
+    key_t key_id;
+    int msgid, recval;
+
+    key_id = ftok("file.txt", 65);
+    msgid = msgget(key_id, 0666 | IPC_CREAT);
+
+    if (msgid == -1)
+    {
+        perror("there error in creates a queue !");
+        exit(-1);
+    }
+    printf("Message queue ID is : %d \n", msgid);
+    return msgid;
+}
+int creatShMemory()
+{
+    int key = ftok("file.txt", 67);
+    int shmid = shmget(key, 50, IPC_CREAT | 0666);
+    if ((long)shmid == -1)
+    {
+        perror("Error in creating shm!");
+        exit(-1);
+    }
+
+    return shmid;
+}
+
+int main(int argc, char *argv[])
+{
+    // Schedular Attribute
+    int totalProcessNum, activateProcess;
+    // initiate Clock
+    // initClk();
+    // Signals Handlers
+    // signal(SIGINT,HandlerINT);
+    // signal(SIGCHLD,HandlerCHILD);
+
+    /// get type of schedular from process_generator using message queue
+
+    struct schdularType schedularmessage;
+    int done;
+
+    int msgid = createMessageQueue();
+    done = msgrcv(msgid, &schedularmessage, sizeof(schedularmessage.schedType), 0, !IPC_NOWAIT);
+
+    if (done == -1)
+    {
+        perror("error in reciving ?");
+    }
+    else
+    {
+        printf("the message reseved in : %s\n", schedularmessage.schedType);
+    }
+    msgctl(msgid, IPC_RMID, (struct msqid_ds *)0);
+
+    struct processData *P = (struct processData *)malloc(sizeof(struct processData));
+
+    int shmid = creatShMemory();
+    void *shmaddr = shmat(shmid, (void *)0, 0);
+    if (shmaddr == (void *)-1)
+    {
+        perror("Error in attach in reader");
+        exit(-1);
+    }
+
+    printf("\nReader: Shared memory attached at address %p\n", shmaddr);
+
+    P = (struct processData *)shmaddr;
+
+    printf("the message reseved in : %d %d %d %d \n", P->arrival, P->id, P->priority, P->runtime);
+
+    strcpy((char *)shmaddr, "quit");
+    shmdt(shmaddr);
+
+    // TODO implement the scheduler :)
+    // upon termination release the clock resources.
+
+    // destroyClk(true);
+    return 0;
 }
